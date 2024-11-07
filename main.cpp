524e9cc1d4ad100b5bba928e46cda5b85109f5cf
EzizBallyev <ballyev.2801@gmail.com>
2024-11-07 21:48:39 +0000

    New

--- main.cpp
+++ main.cpp
@@ -1,22 +1,113 @@
 #include <iostream>
-#include "bmp.h"
+#include <fstream>
+#include <cstdint>
 
-int main() {
-    try {
-        BMP image("input.bmp");
+#pragma pack(push, 1)
+struct BMPHeader {
+    uint16_t fileType{0x4D42};
+    uint32_t fileSize{0};
+    uint16_t reserved1{0};
+    uint16_t reserved2{0};
+    uint32_t offsetData{0};
+};
 
-        image.Rotate90();
-        image.Save("rotated90.bmp");
+struct BMPInfoHeader {
+    uint32_t size{0};
+    int32_t width{0};
+    int32_t height{0};
+    uint16_t planes{1};
+    uint16_t bitCount{0};
+    uint32_t compression{0};
+    uint32_t sizeImage{0};
+    int32_t xPixelsPerMeter{0};
+    int32_t yPixelsPerMeter{0};
+    uint32_t colorsUsed{0};
+    uint32_t colorsImportant{0};
+};
+#pragma pack(pop)
 
-        image.RotateCounter90();
-        image.Save("rotatedCounter90.bmp");
+struct Pixel {
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+};
 
-        image.GaussianFilter();
-        image.Save("filtered.bmp");
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
+class PictureBMP {
+public:
+    PictureBMP(const std::string &filename);
+    ~PictureBMP();
+    void Save(const std::string &filename);
+    void Rotate90();
+    void RotateCounter90();
+    void GaussianFilter();
+
+private:
+    BMPHeader header;
+    BMPInfoHeader infoHeader;
+    Pixel** data;
+    bool Memory(int height, int width);
+    void FreeMemory(int height);
+};
+
+bool PictureBMP::Memory(int height,int width) {
+    data = new (std::nothrow) Pixel*[height];
+    if (!data) return false;
+
+    for (int i = 0; i < height; ++i) {
+        data[i] = new (std::nothrow) Pixel[width];
+        if (!data[i]) {
+            for (int j = 0; j < i; ++j) {
+                delete[] data[j];
+            }
+            delete[] data;
+            data = nullptr;
+            return false;
+        }
+    }
+    return true;
+}
+
+void PictureBMP::FreeMemory(int height) {
+    if (data) {
+        for (int i = 0; i < height; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+        data = nullptr;
+    }
+}
+
+
+PictureBMP::PictureBMP(const std::string &filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        throw std::runtime_error("Error open file.");
     }
 
-    return 0;
-}
+    file.read(reinterpret_cast<char *>(&header), sizeof(header));
+    if (header.fileType != 0x4D42) {
+        throw std::runtime_error("File format is not BMP.");
+    }
+
+    file.read(reinterpret_cast<char *>(&infoHeader), sizeof(infoHeader));
+
+    infoHeader.height = std::abs(infoHeader.height);
+    infoHeader.width = std::abs(infoHeader.width);
+
+    if (infoHeader.width == 0 || infoHeader.height == 0) {
+        throw std::runtime_error("unexceptable file size.");
+    }
+    file.seekg(header.offsetData, file.beg);
+
+
+    Memory(infoHeader.height, infoHeader.width);
+
+
+    for (int i = 0; i < infoHeader.height; ++i) {
+        for (int j = 0; j < infoHeader.width; ++j) {
+            file.read(reinterpret_cast<char *>(&data[i][j]), sizeof(Pixel));
+            if (!file) {
+                throw std::runtime_error("Error file read.");
+            }
+        }
+    }
